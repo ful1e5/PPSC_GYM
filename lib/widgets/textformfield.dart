9fd4c7c8c1c59ed75399462bd64c920e6188787b
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-05-07 17:20:14 +0530

    🏷️Controller param added

--- lib/widgets/textformfield.dart
+++ lib/widgets/textformfield.dart
@@ -2,17 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class TextFormFieldWidget extends StatelessWidget {
-  final String? labelText;
   final List<TextInputFormatter>? formatters;
   final TextInputType? keyboardType;
+  final String? labelText;
+  final int? maxLength;
+  final TextEditingController? controller;
+  final String? initialValue;
   final FormFieldValidator<String>? validator;
+  final GestureTapCallback? onTap;
 
   const TextFormFieldWidget(
       {Key? key,
-      @required this.labelText,
       this.formatters,
       @required this.keyboardType,
-      this.validator})
+      this.maxLength,
+      @required this.labelText,
+      this.controller,
+      this.initialValue,
+      this.validator,
+      this.onTap})
       : super(key: key);
 
   @override
@@ -20,18 +28,30 @@ class TextFormFieldWidget extends StatelessWidget {
     return (TextFormField(
         inputFormatters: formatters,
         keyboardType: keyboardType,
+        maxLength: maxLength,
+        controller: controller,
+        initialValue: initialValue,
         decoration: InputDecoration(
           labelText: labelText,
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(12.0)),
-            borderSide: BorderSide(color: Colors.blue, width: 2),
+            borderSide: BorderSide(color: Colors.white24, width: 1),
           ),
           focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            borderSide: BorderSide(color: Colors.blue),
+            borderRadius: BorderRadius.all(Radius.circular(14.0)),
+            borderSide: BorderSide(color: Colors.blue, width: 2),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(14.0)),
+            borderSide: BorderSide(color: Colors.red, width: 2),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(14.0)),
+            borderSide: BorderSide(color: Colors.red, width: 2),
           ),
         ),
         // The validator receives the text that the user has entered.
-        validator: validator));
+        validator: validator,
+        onTap: onTap));
   }
 }

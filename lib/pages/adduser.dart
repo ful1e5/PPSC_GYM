09d8ce9d8e002e196b9d0ec3cb9488ca00ed4472
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-05-07 17:20:47 +0530

    🔥 DOB field init

--- lib/pages/adduser.dart
+++ lib/pages/adduser.dart
@@ -10,6 +10,17 @@ class AddUserPage extends StatefulWidget {
 
 class _AddUserPageState extends State<AddUserPage> {
   final _formKey = GlobalKey<FormState>();
+
+  final _dobCtrl = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    _dobCtrl.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +28,7 @@ class _AddUserPageState extends State<AddUserPage> {
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: Colors.transparent,
-          title: const Text('Add User'),
+          title: const Text('Add Client'),
           actions: <Widget>[
             IconButton(
               icon: const Icon(Icons.check),
@@ -37,13 +48,63 @@ class _AddUserPageState extends State<AddUserPage> {
         body: Form(
             key: _formKey,
             child: Padding(
-              padding: EdgeInsets.all(25.0),
-              child: Column(
-                children: <Widget>[
-                  TextFormFieldWidget(
-                      labelText: "Adhar ID", keyboardType: TextInputType.number)
-                ],
-              ),
-            )));
+                padding: EdgeInsets.all(25.0),
+                child: Column(
+                  children: <Widget>[
+                    TextFormFieldWidget(
+                      labelText: "Adhar ID",
+                      maxLength: 12,
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            value.length != 12) {
+                          return 'Invalid ID';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormFieldWidget(
+                      labelText: "Name",
+                      keyboardType: TextInputType.text,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'This field is required!';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormFieldWidget(
+                        controller: _dobCtrl,
+                        labelText: "Date Of Birth",
+                        keyboardType: TextInputType.text,
+                        onTap: () async {
+                          // Below line stops keyboard from appearing
+                          FocusScope.of(context).requestFocus(new FocusNode());
+
+                          final DateTime? date = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(1975),
+                              lastDate: DateTime(2024));
+
+                          if (date != null)
+                            setState(
+                              () {
+                                _dobCtrl.text =
+                                    "${date.day.toString().padLeft(2, '0')}-${date.month.toString().padLeft(2, '0')}-${date.year.toString()}";
+                              },
+                            );
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'This field is required!';
+                          }
+                          return null;
+                        })
+                  ],
+                ))));
   }
 }

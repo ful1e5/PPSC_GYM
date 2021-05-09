4449b5ca64226c05054372f532d52b898d862025
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-05-09 17:47:33 +0530

    ✨ Advance selection

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,12 +17,21 @@ class _HomePageState extends State<HomePage> {
   late DatabaseHandler handler;
   late Future<List<Client>> _future;
 
+  late Map<int, bool> selectedFlag;
+  late bool isSelectionMode;
+
   @override
   void initState() {
     super.initState();
 
     handler = DatabaseHandler();
     _refreshData(3);
+    _resetSelection();
+  }
+
+  void _resetSelection() {
+    isSelectionMode = false;
+    selectedFlag = {};
   }
 
   void _refreshData(int seconds) {
@@ -33,64 +42,67 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        centerTitle: true,
-        title: const Text('Home'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.more_vert),
-            tooltip: 'Menu',
-            onPressed: () {
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(const SnackBar(content: Text('Menu')));
-            },
+    return WillPopScope(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            centerTitle: true,
+            title: const Text('Home'),
+            actions: _buildActions(),
           ),
-        ],
-      ),
-      body: RefreshIndicator(
-          backgroundColor: Colors.white,
-          color: Colors.black,
-          onRefresh: () async {
-            setState(() {
-              _refreshData(2);
-            });
-            return;
-          },
-          child: FutureBuilder(
-              future: _future,
-              builder:
-                  (BuildContext context, AsyncSnapshot<List<Client>> snapshot) {
-                if (snapshot.connectionState != ConnectionState.done) {
-                  return _buildLoader();
-                }
-                if (snapshot.hasError) {
-                  return Center(child: Text("Error !", style: slightBoldText));
-                }
-                if (snapshot.hasData) {
-                  return _buildListView(snapshot);
-                }
-                return _buildNoData();
-              })),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final String? newClient = await Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AddUserPage()),
-          );
+          body: RefreshIndicator(
+              backgroundColor: Colors.white,
+              color: Colors.black,
+              onRefresh: () async {
+                setState(() {
+                  _refreshData(2);
+                });
+                return;
+              },
+              child: FutureBuilder(
+                  future: _future,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<Client>> snapshot) {
+                    if (snapshot.connectionState != ConnectionState.done) {
+                      return _buildLoader();
+                    }
+                    if (snapshot.hasError) {
+                      return Center(
+                          child: Text("Error !", style: slightBoldText));
+                    }
+                    if (snapshot.hasData) {
+                      return _buildListView(snapshot);
+                    }
+                    return _buildNoData();
+                  })),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () async {
+              final String? newClient = await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddUserPage()),
+              );
 
-          if (newClient == "added") {
+              if (newClient == "added") {
+                setState(() {
+                  _refreshData(0);
+                });
+              }
+            },
+            child: const Icon(Icons.add_rounded),
+            backgroundColor: Colors.green,
+          ),
+        ),
+        onWillPop: () async {
+          if (isSelectionMode) {
             setState(() {
-              _refreshData(0);
+              _resetSelection();
             });
+            return false;
+          } else {
+            return true;
           }
-        },
-        child: const Icon(Icons.add_rounded),
-        backgroundColor: Colors.green,
-      ),
-    );
+        });
   }
 
   Widget _buildLoader() {
@@ -108,25 +120,105 @@ class _HomePageState extends State<HomePage> {
       return ListView.builder(
         itemCount: snapshot.data?.length,
         itemBuilder: (BuildContext context, int index) {
-          return _buildItem(snapshot.data![index].name);
+          selectedFlag[index] = selectedFlag[index] ?? false;
+          bool isSelected = selectedFlag[index] ?? false;
+
+          return Card(
+            color: Colors.transparent,
+            margin: EdgeInsets.all(9),
+            semanticContainer: true,
+            child: ListTile(
+              onLongPress: () => onLongPress(isSelected, index),
+              onTap: () => onTap(isSelected, index),
+              leading: _buildSelectIcon(isSelected),
+              title: Text(snapshot.data![index].name,
+                  style:
+                      TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+            ),
+          );
         },
       );
     }
   }
 
-  Widget _buildItem(String title) {
-    return Card(
-      color: Colors.transparent,
-      margin: EdgeInsets.all(9),
-      semanticContainer: true,
-      child: ListTile(
-        leading: CircleAvatar(
-            radius: 22.0,
-            backgroundColor: Colors.white24,
-            child: const Icon(Icons.person, size: 25, color: Colors.white54)),
-        title: Text(title,
-            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
-      ),
-    );
+  void onLongPress(bool isSelected, int index) {
+    setState(() {
+      selectedFlag[index] = !isSelected;
+      // If there will be any true in the selectionFlag then
+      // selection Mode will be true
+      isSelectionMode = selectedFlag.containsValue(true);
+    });
+  }
+
+  Widget _buildSelectIcon(bool isSelected) {
+    if (isSelectionMode) {
+      return CircleAvatar(
+          radius: 22.0,
+          backgroundColor: isSelected ? Colors.white : Colors.white24,
+          child: isSelected
+              ? const Icon(Icons.check, size: 25, color: Colors.black)
+              : null);
+    } else {
+      return CircleAvatar(
+          radius: 22.0,
+          backgroundColor: Colors.white24,
+          child: const Icon(Icons.person, size: 25, color: Colors.white54));
+    }
+  }
+
+  void onTap(bool isSelected, int index) {
+    if (isSelectionMode) {
+      setState(() {
+        selectedFlag[index] = !isSelected;
+        isSelectionMode = selectedFlag.containsValue(true);
+      });
+    } else {
+      // Open Detail Page
+    }
+  }
+
+  List<Widget> _buildActions() {
+    // The button will be visible when the selectionMode is enabled.
+    if (isSelectionMode) {
+      bool isFalseAvailable = selectedFlag
+          .containsValue(false); // check if all item is not selected
+      return [
+        IconButton(
+          tooltip: "Delete",
+          onPressed: () {
+            print("deleting");
+          },
+          icon: Icon(Icons.delete),
+        ),
+        IconButton(
+          tooltip: isFalseAvailable ? "SelectAll" : "Clear",
+          onPressed: _selectAll,
+          icon: Icon(
+            isFalseAvailable ? Icons.done_all : Icons.remove_done,
+          ),
+        )
+      ];
+    } else {
+      return [
+        IconButton(
+          icon: const Icon(Icons.more_vert),
+          tooltip: 'Menu',
+          onPressed: () {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(const SnackBar(content: Text('Menu')));
+          },
+        )
+      ];
+    }
+  }
+
+  void _selectAll() {
+    bool isFalseAvailable = selectedFlag.containsValue(false);
+    // If false will be available then it will select all the checkbox
+    // If there will be no false then it will de-select all
+    selectedFlag.updateAll((key, value) => isFalseAvailable);
+    setState(() {
+      isSelectionMode = selectedFlag.containsValue(true);
+    });
   }
 }

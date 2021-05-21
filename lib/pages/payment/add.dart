8a3085071ef593fbda2df2aebaee30d34f3f3c60
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-05-21 10:53:45 +0530

    📢 Dialog style updated & refactored with local methods

--- lib/pages/payment/add.dart
+++ lib/pages/payment/add.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'package:ppscgym/widgets.dart';
-import 'package:ppscgym/utils.dart';
-
 import 'package:ppscgym/services/database/handler.dart';
 import 'package:ppscgym/services/database/models.dart';
+
+import 'package:ppscgym/styles.dart';
+import 'package:ppscgym/widgets.dart';
+import 'package:ppscgym/utils.dart';
 
 class AddPaymentPage extends StatefulWidget {
   final int clientId;
@@ -18,35 +19,37 @@ class AddPaymentPage extends StatefulWidget {
 }
 
 class _AddPaymentPageState extends State<AddPaymentPage> {
-  final _formKey = GlobalKey<FormState>();
+  final formKey = GlobalKey<FormState>();
 
   final DateTime todayDate = DateTime.now();
-  late DateTime firstDayOfMonthDate;
+  late DateTime firstDayOfMonth;
+  late DateTime selectionDefaultDate;
 
-  late bool _fromToday = false;
-  late bool _moneyFromPlan = true;
-  late bool _addNote = false;
+  late bool fromToday = false;
+  late bool moneyFromPlan = true;
+  late bool addNote = false;
 
-  late DateTime selectionDefaultDate = todayDate;
-  final _startDateCtrl = TextEditingController();
-  final _endDateCtrl = TextEditingController();
-  final _moneyCtrl = TextEditingController();
-  final _noteCtrl = TextEditingController();
+  final startdateCtrl = TextEditingController();
+  final enddateCtrl = TextEditingController();
+  final moneyCtrl = TextEditingController();
+  final noteCtrl = TextEditingController();
 
   @override
   void initState() {
     super.initState();
 
-    firstDayOfMonthDate = DateTime(todayDate.year, todayDate.month, 1);
-    setDateCtrls(firstDayOfMonthDate);
+    firstDayOfMonth = DateTime(todayDate.year, todayDate.month, 1);
+    selectionDefaultDate = todayDate;
+    setDateCtrls(firstDayOfMonth);
 
-    _moneyCtrl.text = widget.plan.price.toString();
+    moneyCtrl.text = widget.plan.price.toString();
   }
 
   void setDateCtrls(DateTime date) {
-    _startDateCtrl.text = toDDMMYYYY(date);
-    _endDateCtrl.text = toDDMMYYYY(
-        DateTime(date.year, date.month + widget.plan.months, date.day));
+    startdateCtrl.text = toDDMMYYYY(date);
+    enddateCtrl.text = toDDMMYYYY(
+      DateTime(date.year, date.month + widget.plan.months, date.day),
+    );
     selectionDefaultDate = date;
   }
 
@@ -59,272 +62,270 @@ class _AddPaymentPageState extends State<AddPaymentPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.transparent,
-        title: const Text('Review Payment'),
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {
-              // Validate returns true if the form is valid, or false otherwise.
-              if (_formKey.currentState!.validate()) {
-                addWarningDialog();
-              }
-            },
-            icon: Icon(Icons.check),
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Padding(
-            padding: EdgeInsets.all(25.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                //
-                // Status
-                //
+      appBar: buildAppBar(),
+      body: buildBody(),
+    );
+  }
 
-                Text('${widget.plan.months} Month Plan',
-                    style:
-                        TextStyle(fontSize: 37.0, fontWeight: FontWeight.bold)),
-                gapWidget(30.0),
+  AppBar buildAppBar() {
+    return AppBar(
+      centerTitle: true,
+      backgroundColor: Colors.transparent,
+      title: const Text('Review Payment'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.check),
+          onPressed: () {
+            // Validate returns true if the form is valid, or false otherwise.
+            if (formKey.currentState!.validate()) {
+              warningDialog();
+            }
+          },
+        )
+      ],
+    );
+  }
 
-                //
-                // Starting date
-                //
+  Widget buildBody() {
+    return SingleChildScrollView(
+      child: Form(
+        key: formKey,
+        child: Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              //
+              // Status
+              //
 
-                SwitchListTile(
-                  title: const Text('From Today'),
-                  value: _fromToday,
-                  onChanged: (bool value) {
-                    setState(() {
-                      _fromToday = value;
-                      if (value == true) {
-                        setDateCtrls(todayDate);
-                      } else {
-                        setDateCtrls(firstDayOfMonthDate);
-                      }
-                    });
-                  },
+              Text(
+                '${widget.plan.months} Month Plan',
+                style: TextStyle(
+                  fontSize: 37.0,
+                  fontWeight: FontWeight.bold,
                 ),
-                gapWidget(20.0),
+              ),
+              gapWidget(30.0),
 
-                TextFormFieldWidget(
-                  labelText: 'Starting Date',
-                  enabled: !_fromToday,
-                  controller: _startDateCtrl,
-                  enableInteractiveSelection: false,
-                  keyboardType: TextInputType.text,
-                  onTap: () async {
-                    // Below line stops keyboard from appearing
-                    FocusScope.of(context).requestFocus(new FocusNode());
-                    DateTime? date = await pickDate(context,
-                        initialDate: selectionDefaultDate);
-                    if (date != null)
-                      setState(() {
-                        setDateCtrls(date);
-                      });
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'This Field is required';
-                    } else if (stringToDateTime(value)
-                        .isBefore(firstDayOfMonthDate)) {
-                      return 'Date from previous months is not allowed';
+              //
+              // Starting date
+              //
+
+              SwitchListTile(
+                value: fromToday,
+                title: const Text('From Today'),
+                onChanged: (bool value) {
+                  setState(() {
+                    fromToday = value;
+                    if (value == true) {
+                      setDateCtrls(todayDate);
+                    } else {
+                      setDateCtrls(firstDayOfMonth);
                     }
-                    return null;
-                  },
-                ),
-                gapWidget(20.0),
+                  });
+                },
+              ),
+              gapWidget(20.0),
 
-                //
-                // Ending date
-                //
-
-                TextFormFieldWidget(
-                  enabled: false,
-                  labelText: 'Ending Date',
-                  controller: _endDateCtrl,
-                  enableInteractiveSelection: false,
-                  keyboardType: TextInputType.text,
-                ),
-                gapWidget(20.0),
-
-                SwitchListTile(
-                  title: const Text('Money From Plan'),
-                  value: _moneyFromPlan,
-                  onChanged: (bool value) {
+              TextFormFieldWidget(
+                enabled: !fromToday,
+                labelText: 'Starting Date',
+                controller: startdateCtrl,
+                enableInteractiveSelection: false,
+                keyboardType: TextInputType.text,
+                onTap: () async {
+                  // Below line stops keyboard from appearing
+                  FocusScope.of(context).requestFocus(new FocusNode());
+                  DateTime? date = await pickDate(
+                    context,
+                    initialDate: selectionDefaultDate,
+                  );
+                  if (date != null) {
                     setState(() {
-                      _moneyFromPlan = value;
-                      if (_moneyFromPlan == true) {
-                        _moneyCtrl.text = widget.plan.price.toString();
-                      }
+                      setDateCtrls(date);
                     });
-                  },
-                ),
-                gapWidget(20.0),
+                  }
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'This Field is required';
+                  } else if (stringToDateTime(value)
+                      .isBefore(firstDayOfMonth)) {
+                    return 'Date from previous months is not allowed';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              gapWidget(20.0),
 
-                //
-                // Money
-                //
+              //
+              // Ending date
+              //
 
-                TextFormFieldWidget(
-                  enabled: !_moneyFromPlan,
-                  labelText: 'Money',
-                  controller: _moneyCtrl,
-                  autoFocus: true,
-                  formatters: [
-                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
-                  ],
-                  maxLength: 5,
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'This field required';
-                    } else if (int.parse(value) > 50000) {
-                      return 'Max Price set to 50,000\u20B9';
+              TextFormFieldWidget(
+                enabled: false,
+                labelText: 'Ending Date',
+                controller: enddateCtrl,
+                enableInteractiveSelection: false,
+                keyboardType: TextInputType.text,
+              ),
+              gapWidget(20.0),
+
+              SwitchListTile(
+                value: moneyFromPlan,
+                title: const Text('Money From Plan'),
+                onChanged: (bool value) {
+                  setState(() {
+                    moneyFromPlan = value;
+                    if (moneyFromPlan == true) {
+                      moneyCtrl.text = widget.plan.price.toString();
                     }
+                  });
+                },
+              ),
+              gapWidget(20.0),
+
+              //
+              // Money
+              //
+
+              TextFormFieldWidget(
+                enabled: !moneyFromPlan,
+                maxLength: 5,
+                labelText: 'Money',
+                controller: moneyCtrl,
+                keyboardType: TextInputType.number,
+                formatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
+                ],
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'This field required';
+                  } else if (int.parse(value) > 50000) {
+                    return 'Max Price set to 50,000\u20B9';
+                  } else {
                     return null;
-                  },
-                ),
-                gapWidget(20.0),
+                  }
+                },
+              ),
+              gapWidget(20.0),
 
-                //
-                // Note
-                //
+              //
+              // Note
+              //
 
-                SwitchListTile(
-                  title: const Text('Add Note'),
-                  value: _addNote,
-                  onChanged: (bool value) {
-                    setState(() {
-                      _addNote = value;
-                    });
-                  },
-                ),
-                gapWidget(20.0),
+              SwitchListTile(
+                title: const Text('Add Note'),
+                value: addNote,
+                onChanged: (bool value) {
+                  setState(() {
+                    addNote = value;
+                  });
+                },
+              ),
+              gapWidget(20.0),
 
-                TextFormFieldWidget(
-                    labelText: 'Note',
-                    enabled: _addNote,
-                    controller: _noteCtrl,
-                    formatters: [
-                      FilteringTextInputFormatter.allow(
-                        RegExp(r'^[a-zA-Z0-9\s-._]*$'),
-                      ),
-                    ],
-                    keyboardType: TextInputType.text,
-                    validator: (value) {
-                      if (_addNote == true &&
-                          (value == null || value.isEmpty)) {
-                        return 'Empty note not allowed.';
-                      }
-                      return null;
-                    }),
-              ],
-            ),
+              TextFormFieldWidget(
+                labelText: 'Note',
+                enabled: addNote,
+                controller: noteCtrl,
+                formatters: [
+                  FilteringTextInputFormatter.allow(
+                    RegExp(r'^[a-zA-Z0-9\s-._]*$'),
+                  ),
+                ],
+                keyboardType: TextInputType.text,
+                validator: (value) {
+                  if (addNote == true && (value == null || value.isEmpty)) {
+                    return 'Empty note not allowed.';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 
-  addWarningDialog() {
+  warningDialog() {
     return showDialog(
       context: context,
       builder: (BuildContext context) {
         return Dialog(
+          backgroundColor: Colors.blue,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(25.0),
-            ),
-            side: BorderSide(
-              width: 1.5,
-              color: Colors.white10,
-            ),
+            borderRadius: BorderRadius.all(Radius.circular(40.0)),
           ),
-          backgroundColor: Colors.black87,
           child: Container(
-            height: 300,
+            height: 310,
             color: Colors.transparent,
-            child: Padding(padding: EdgeInsets.all(15), child: buildWarning()),
+            child: Padding(
+              padding: EdgeInsets.all(25.0),
+              child: warningDialogView(),
+            ),
           ),
         );
       },
     );
   }
 
-  Widget buildWarning() {
+  Widget warningDialogView() {
     return Column(
       children: [
         Center(
-            child:
-                Icon(Icons.warning, size: 100.0, color: Colors.yellowAccent)),
-        Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(14.0),
-          child: Text(
-            "You Can't able to edit or delete payment information, once it's verified by algorithm. Tap 'I Agree' to proceed.",
-            overflow: TextOverflow.visible,
-            style: TextStyle(fontSize: 12.0, wordSpacing: 2.0),
+          child: Icon(
+            Icons.security_rounded,
+            size: 80.0,
           ),
         ),
-        Expanded(
-          child: Row(
-            children: [
-              OutlinedButton(
-                child: const Text('Close'),
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.black),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0))),
-                ),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-              Spacer(),
-              OutlinedButton(
-                  child: const Text('I Agree'),
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blue),
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30.0))),
-                  ),
-                  onPressed: () async {
-                    Payment payment = Payment(
-                        clientId: widget.clientId,
-                        months: widget.plan.months,
-                        startDate: _startDateCtrl.text,
-                        endDate: _endDateCtrl.text,
-                        money: int.parse(_moneyCtrl.text),
-                        note: (_addNote == true) ? _noteCtrl.text : null);
-
-                    await insertPayment(payment);
-
-                    //TODO: use widgets popups
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        backgroundColor: Colors.green,
-                        content:
-                            Text('${widget.plan.months} Months Plan Active')));
-
-                    Navigator.pop(context); // Popup Dialog
-
-                    // TODO: 'payment added'
-                    Navigator.pop(context, 'added'); // Popup Widget
-                  }),
-            ],
+        gapWidget(15.0),
+        Text(
+          "You Can't able to edit or delete payment information, "
+          "once it's verified by algorithm. "
+          "Tap 'I Agree' to proceed.",
+          overflow: TextOverflow.visible,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 11.3,
           ),
-        )
+        ),
+        gapWidget(50.0),
+        OutlinedButton(
+          child: const Text(
+            'I Agree',
+            style: TextStyle(
+              fontSize: 15.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          style: materialButtonStyle(),
+          onPressed: () async {
+            Payment payment = Payment(
+                clientId: widget.clientId,
+                months: widget.plan.months,
+                startDate: startdateCtrl.text,
+                endDate: enddateCtrl.text,
+                money: int.parse(moneyCtrl.text),
+                note: (addNote == true) ? noteCtrl.text : null);
+
+            await insertPayment(payment);
+
+            //TODO: use widgets popups
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                backgroundColor: Colors.green,
+                content: Text('${widget.plan.months} Months Plan Active')));
+
+            Navigator.pop(context); // Popup Dialog
+
+            // TODO: 'payment added'
+            Navigator.pop(context, 'added'); // Popup Widget
+          },
+        ),
       ],
     );
   }
--- lib/styles.dart
+++ lib/styles.dart
@@ -2,3 +2,24 @@ import 'package:flutter/material.dart';
 
 final TextStyle slightBoldText = TextStyle(
     fontSize: 16.0, color: Colors.white12, fontWeight: FontWeight.bold);
+
+ButtonStyle materialButtonStyle(
+    {Color? backgroundColor,
+    Color? foregroundColor,
+    EdgeInsetsGeometry? padding}) {
+  return ButtonStyle(
+    backgroundColor:
+        MaterialStateProperty.all<Color>(backgroundColor ?? Colors.white),
+    foregroundColor:
+        MaterialStateProperty.all<Color>(foregroundColor ?? Colors.blue),
+    padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+      padding ??
+          EdgeInsets.only(top: 10.0, bottom: 10.0, right: 40.0, left: 40.0),
+    ),
+    shape: MaterialStateProperty.all(
+      RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+    ),
+  );
+}

d6346c30090699bc190d990284955f7eae58fc5b
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-05-26 16:03:50 +0530

    🔥 Tap action init

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 
+import 'package:flutter/material.dart';
+import 'package:ppscgym/services/database/handler.dart';
+
+import 'package:ppscgym/pages/plans.dart';
 import 'package:ppscgym/widgets.dart';
 
 class SettingPage extends StatefulWidget {
@@ -10,6 +15,11 @@ class SettingPage extends StatefulWidget {
 }
 
 class _SettingPageState extends State<SettingPage> {
+  Future<String?> backup() async {
+    final handler = DatabaseHandler();
+    return await handler.backup();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -30,11 +40,13 @@ class _SettingPageState extends State<SettingPage> {
               cardWidget(
                 icon: Icons.bar_chart,
                 title: Text('Customize Plan'),
+                onTap: gotoPlanPage,
               ),
               sectionTag(tag: 'Backup'),
               cardWidget(
                 icon: Icons.backup_rounded,
                 title: Text('Backup'),
+                onTap: takeBackup,
               ),
               cardWidget(
                 icon: Icons.settings_backup_restore_rounded,
@@ -85,5 +97,22 @@ class _SettingPageState extends State<SettingPage> {
         ),
       ),
     );
+  }
+
+  gotoPlanPage() async {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => PlansPage()),
+    );
+  }
+
+  takeBackup() async {
+    String? backupDir = await FilePicker.platform.getDirectoryPath();
+
+    if (backupDir != null) {
+      final String? csvData = await backup();
+      print(csvData);
+      print(backupDir);
+    }
   }
 }

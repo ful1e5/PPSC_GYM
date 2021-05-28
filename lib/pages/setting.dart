77daf5515438ae551a2adbaa52ba26a81061fe9f
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-05-28 17:30:02 +0530

    ⭐ Use backup dialogs

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:ppscgym/services/backup.dart';
 
 import 'package:ppscgym/pages/plans.dart';
-import 'package:ppscgym/styles.dart';
 import 'package:ppscgym/widgets.dart';
 
 class SettingPage extends StatefulWidget {
@@ -41,13 +40,15 @@ class _SettingPageState extends State<SettingPage> {
                 icon: Icons.backup_rounded,
                 title: Text('Backup'),
                 onTap: () async {
-                  await takeBackup(context);
+                  await backupDialog(context);
                 },
               ),
               cardWidget(
                 icon: Icons.settings_backup_restore_rounded,
                 title: Text('Restore'),
-                onTap: restoreDialog,
+                onTap: () async {
+                  await restoreDialog(context);
+                },
               ),
             ],
           ),
@@ -100,84 +101,6 @@ class _SettingPageState extends State<SettingPage> {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => PlansPage()),
-    );
-  }
-
-  restoreDialog() {
-    return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return Dialog(
-          backgroundColor: Colors.blueGrey,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(40.0)),
-          ),
-          child: Container(
-            color: Colors.transparent,
-            child: Padding(
-              padding: EdgeInsets.all(25.0),
-              child: restoreDialogView(),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  Widget restoreDialogView() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Icon(
-          Icons.restore,
-          size: 80.0,
-        ),
-        gapWidget(15.0),
-        Text(
-          'Choose Restore behaviour. This operation affect existing data. '
-          "Both restoration operations are behaves same, If no data exists.",
-          overflow: TextOverflow.visible,
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 11.3),
-        ),
-        gapWidget(50.0),
-        OutlinedButton(
-          child: const Text(
-            'Merge',
-            style: TextStyle(
-              fontSize: 15.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          style: materialButtonStyle(
-            foregroundColor: Colors.blueGrey,
-            padding: EdgeInsets.symmetric(horizontal: 65.0, vertical: 12.0),
-          ),
-          onPressed: () async {
-            await restoreBackup(context);
-          },
-        ),
-        gapWidget(10.0),
-        OutlinedButton(
-          child: const Text(
-            'Replace',
-            style: TextStyle(
-              fontSize: 15.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          style: materialButtonStyle(
-            foregroundColor: Colors.white,
-            backgroundColor: Colors.black54,
-            padding: EdgeInsets.symmetric(horizontal: 65.0, vertical: 12.0),
-          ),
-          onPressed: () async {
-            await restoreBackup(context, replace: true);
-          },
-        ),
-      ],
     );
   }
 }
